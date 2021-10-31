d6f41a43f6edaf65e7ce3986c62b01cbeda804e2
sam <samcodesit@gmail.com>
2021-10-31 20:21:33 +0000

    Tweak repo fetcher script and readme

--- README.md
+++ README.md
@@ -10,7 +10,9 @@ Top level repository for the [Geometrize](https://www.geometrize.co.uk/) project
 
 Fetch the entire Geometrize codebase by executing the [get_geometrize_projects.sh](https://github.com/Tw1ddle/geometrize-top-level-repo/blob/master/get_geometrize_projects.sh) script.
 
-The Geometrize project is split across numerous git repositories and submodules, since it comprises several separate software projects, and for modularity.
+The Geometrize project is split across numerous git repositories and submodules, since it comprises several separate software projects, and for the sake of modularity.
+
+To contribute, please make a pull request or open an issue on the relevant repository. Write access to repositories can also be granted to trusted contributors.
 
 ## Notes
  * Got an idea or suggestion? Open an issue on GitHub, or send Sam a message on [Twitter](https://twitter.com/Sam_Twidale).
--- get_geometrize_projects.sh
+++ get_geometrize_projects.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-## Script to check out all of the Geometrize project repositories
+localCheckoutFolder="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" ## The base folder to check the Geometrize projects out to (defaults to the current directory the script is in)
 
-localFolderBase="TODO"
-sshUrlBase="git@github.com:Tw1ddle/"
+urlBase="git@github.com:Tw1ddle/" ## Base URL for checking out all projects (from my account on GitHub)
 
-## List of all the Geometrize project repo names
-declare -a repoList=("geometrize" "geometrize-docs" "geometrize-functional-tests", "geometrize-gallery", "geometrize-haxe",
- "geometrize-haxe-example", "geometrize-haxe-unit-tests", "geometrize-haxe-web", "geometrize-lib-docs", "geometrize-lib-example", "geometrize-lib-fuzzing",
- "geometrize-lib-unit-tests", "geometrize-resources", "geometrize-s3-bucket-downloader", "geometrize-screenshots", "geometrize-tween-optimizer", "geometrize-tweens",
- "geometrize-twitter-bot", "geometrize-twitter-bot-docs", "geometrize-website")
+## List of all the Geometrize project repository names
+declare -a repoList=("geometrize" "geometrize-docs" "geometrize-functional-tests" "geometrize-gallery" "geometrize-haxe"
+ "geometrize-haxe-example" "geometrize-haxe-unit-tests" "geometrize-haxe-web" "geometrize-lib-docs" "geometrize-lib-example" "geometrize-lib-fuzzing"
+ "geometrize-lib-unit-tests" "geometrize-resources" "geometrize-s3-bucket-downloader" "geometrize-screenshots" "geometrize-tween-optimizer" "geometrize-tweens"
+ "geometrize-twitter-bot" "geometrize-twitter-bot-docs" "geometrize-website")
 
-## Loop over repositories
+## Loop over the repositories and check each one out, initializing any submodules
 repoListLength=${#repoList[@]}
 for (( i=0; i<${repoListLength}; i++ ));
 do
   repoName=${repoList[$i]}
-  git clone ${sshUrlBase}${repoName}.git ${localFolderBase}${repoName}
+  checkoutCommand="git clone --recurse-submodules -j8 ${urlBase}${repoName}.git ${localCheckoutFolder}/${repoName}"
+  echo "Will run checkout command: ${checkoutCommand}"
+  ${checkoutCommand}
 done
